8a58ac069fa190079275fd594b90a9121e64770d
DanGould <dcsled@gmail.com>
2017-09-11 14:54:10 -0400

    add start script to initialize docker vols

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script orchestrates the running of breezeserver's docker compose
+
+# create volumes if they don't exist
+docker volume create bitcoin-testnet-data
+docker volume create stratis-testnet-data
+docker volume create breezeserver-shared-testnet-data
+
+# populate shared volume
+docker run -v breezeserver-shared-testnet-data:/home/shared --name helper busybox true
+docker cp breeze.conf helper:/home/shared/breeze.conf
+docker rm helper
+
+# compose the breezeserver services
+docker-compose up --build
