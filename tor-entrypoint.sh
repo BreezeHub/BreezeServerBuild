ccf47590896cd1fdcd2495612ce8fd124905d27a
DanGould <dcsled@gmail.com>
2017-09-13 12:30:25 -0400

    update depreciated flag to run tor

--- tor-entrypoint.sh
+++ tor-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-tor -controlport 9051 -cookieauth 1 -cookieauthfile /home/shared/control_auth_cookie
+tor -controlport 9051 -CookieAuthentication 1 -cookieauthfile /home/shared/control_auth_cookie
 
